d74b39a27bdcdafacd57cf1cc017e77345307d6d
Kanat Bayalbayev <Kanatkz07@mail.ru>
2024-01-04 14:16:33 +0600

    Initial commit

---
+++ app/src/main/java/com/smartappsdev/flashmind/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.smartappsdev.flashmind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
